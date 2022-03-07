6cc354a3ac685b8748547edb695fc5d56520777d
Trevor Rapp <trrapp@gmail.com>
2022-03-06 20:10:53 -0700

    adds some more test to get full test coverage on fizzBuzz and sets up preferece test as well

--- index.test.js
+++ index.test.js
@@ -5,4 +5,9 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(15)).toBe('fizzBuzz');
     expect(fizzBuzz(30)).toBe('fizzBuzz');
   });
+  it('returns the given number for multiples of neither 3 nor 5' , () => {
+    expect(fizzBuzz(1)).toBe('1');
+    expect(fizzBuzz(22)).toBe('22');
+  })
 });
+
