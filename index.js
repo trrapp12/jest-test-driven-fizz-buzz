455bd7b87e4d23bd118989af0278b57978fdd0c4
Trevor Rapp <trrapp@gmail.com>
2022-03-06 20:32:21 -0700

    adds tests for the rest of fizzBuzz

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // index.js
 
 module.exports = (num) => {
-  if (num % 15 === 0) return 'fizzBuzz';
+  if (num % 15 === 0) {
+    return 'fizzBuzz';
+  } else if (num % 5 === 0) {
+    return 'Buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  }
   return `${num}`
 };
--- index.test.js
+++ index.test.js
@@ -8,6 +8,12 @@ describe('fizzBuzz()', () => {
   it('returns the given number for multiples of neither 3 nor 5' , () => {
     expect(fizzBuzz(1)).toBe('1');
     expect(fizzBuzz(22)).toBe('22');
+  });
+  it('returns fizz on multiples of 3' , () => {
+    expect(fizzBuzz(9)).toBe('fizz')
+  });
+  it('return buzz on multiples of 5' , () => {
+    expect(fizzBuzz(50)).toBe('Buzz')
   })
 });
 
